809a021c0e93e9c4d72b77500e58393081e7d796
alex <alex@levelism.com>
2020-10-08 15:58:04 -0700

    add rendering components to new cell class.

--- Assets/Scripts/CellV2.cs
+++ Assets/Scripts/CellV2.cs
@@ -13,27 +13,47 @@ public enum CellV2FaceDirection
 
 public class FaceInfo
 {
-    
+
 }
 
 public class CellV2 : MonoBehaviour
 {
-
     public float Height = 1f;
+
+    public Material worldMaterial;
 
     void Start()
     {
-        
+        _meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        if (_meshRenderer == null)
+        {
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+
+        if (worldMaterial == null)
+        {
+            _meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+        }
+        else
+        {
+            _meshRenderer.sharedMaterial = worldMaterial;
+        }
+
+        _meshFilter = gameObject.GetComponent<MeshFilter>();
+        if (_meshFilter == null)
+        {
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
     }
 
     void Update()
     {
-        
+
     }
     // logical model data
     Vector3[] _logicalVertices = new Vector3[8];
 
-    Dictionary<CellV2FaceDirection,FaceInfo> _faceInfos = 
+    Dictionary<CellV2FaceDirection, FaceInfo> _faceInfos =
         new Dictionary<CellV2FaceDirection, FaceInfo>();
     // render model data
     Vector3[] _renderVertices = new Vector3[20]; // 5 faces with 4 vertices each
@@ -41,5 +61,10 @@ public class CellV2 : MonoBehaviour
     Vector2[] _renderUVs = new Vector2[20];
     int[] _renderIndices = new int[30]; // 5 faces * (2 triangles * 3 indices)
 
+    MeshFilter _meshFilter;
+
+    Mesh _mesh;
+
+    MeshRenderer _meshRenderer;
 
 }
