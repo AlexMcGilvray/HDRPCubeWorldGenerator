1c80b3b034541f2ec43bdc4372abc986486782ed
alex <alex@levelism.com>
2020-10-08 14:36:49 -0700

    initial

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public Material worldMaterial;
+
+    MeshFilter meshFilter;
+
+    Mesh mesh;
+
+    MeshRenderer meshRenderer;
+
+    Vector3[] vertices = new Vector3[8];
+    Vector3[] normals = new Vector3[8];
+    Vector2[] uv = new Vector2[8];
+    int[] tris = new int[30];
+
+
+    public float Height{get;set;}
+    // we animate from a flat plane to its height. These variables are for tracking height 
+    // information.
+    float animTimeTarget;
+    float animTimeCurrent;
+
+    const float AnimHeightSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        if (meshRenderer == null)
+        {
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        }
+
+        if (worldMaterial == null)
+        {
+            meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+        }
+        else
+        {
+            meshRenderer.sharedMaterial = worldMaterial;
+        }
+
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+        if (meshFilter == null)
+        {
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+    }
+
+    public void MakeCube(float height, int side, float animTime = 1)
+    {
+        Height = height;
+        animTimeTarget = animTime;
+        animTimeCurrent = 0;
+
+        vertices[0].x = -side;
+        vertices[0].y = 0;
+        vertices[0].z = side;
+
+        vertices[1].x = side;
+        vertices[1].y = 0;
+        vertices[1].z = side;
+
+        vertices[2].x = side;
+        vertices[2].y = 0;
+        vertices[2].z = -side;
+
+        vertices[3].x = -side;
+        vertices[3].y = 0;
+        vertices[3].z = -side;
+
+        vertices[4].x = -side;
+        vertices[4].y = 0;
+        vertices[4].z = side;
+
+        vertices[5].x = side;
+        vertices[5].y = 0;
+        vertices[5].z = side;
+
+        vertices[6].x = side;
+        vertices[6].y = 0;
+        vertices[6].z = -side;
+
+        vertices[7].x = -side;
+        vertices[7].y = 0;
+        vertices[7].z = -side;
+
+        const int NumIndexesPerQuad = 6;
+        int currentIndexBase = 0;
+
+        const int NumNormalsPerQuad = 4;
+        int currentNormalBase = 0;
+
+        // top
+        currentIndexBase = NumIndexesPerQuad * 0;
+        tris[currentIndexBase + 0] = 3;
+        tris[currentIndexBase + 1] = 0;
+        tris[currentIndexBase + 2] = 1;
+        tris[currentIndexBase + 3] = 2;
+        tris[currentIndexBase + 4] = 3;
+        tris[currentIndexBase + 5] = 1;
+
+        Vector3 upNormal = Vector3.Cross(vertices[0] - vertices[3], vertices[1] - vertices[3]);
+        upNormal.Normalize();
+
+        currentNormalBase = NumNormalsPerQuad * 0;
+        normals[currentNormalBase + 0] = upNormal;
+        normals[currentNormalBase + 1] = upNormal;
+        normals[currentNormalBase + 2] = upNormal;
+        normals[currentNormalBase + 3] = upNormal;
+
+        // north
+        currentIndexBase = NumIndexesPerQuad * 1;
+        tris[currentIndexBase + 0] = 1;
+        tris[currentIndexBase + 1] = 0;
+        tris[currentIndexBase + 2] = 4;
+        tris[currentIndexBase + 3] = 1;
+        tris[currentIndexBase + 4] = 4;
+        tris[currentIndexBase + 5] = 5;
+
+        // south
+        currentIndexBase = NumIndexesPerQuad * 2;
+        tris[currentIndexBase + 0] = 3;
+        tris[currentIndexBase + 1] = 2;
+        tris[currentIndexBase + 2] = 6;
+        tris[currentIndexBase + 3] = 3;
+        tris[currentIndexBase + 4] = 6;
+        tris[currentIndexBase + 5] = 7;
+
+        // east
+        currentIndexBase = NumIndexesPerQuad * 3;
+        tris[currentIndexBase + 0] = 2;
+        tris[currentIndexBase + 1] = 1;
+        tris[currentIndexBase + 2] = 5;
+        tris[currentIndexBase + 3] = 2;
+        tris[currentIndexBase + 4] = 5;
+        tris[currentIndexBase + 5] = 6;
+
+        // west
+        currentIndexBase = NumIndexesPerQuad * 4;
+        tris[currentIndexBase + 0] = 3;
+        tris[currentIndexBase + 1] = 4;
+        tris[currentIndexBase + 2] = 0;
+        tris[currentIndexBase + 3] = 4;
+        tris[currentIndexBase + 4] = 3;
+        tris[currentIndexBase + 5] = 7;
+
+        normals[0] = -Vector3.forward;
+        normals[1] = -Vector3.forward;
+        normals[2] = -Vector3.forward;
+        normals[3] = -Vector3.forward;
+
+        uv[0] = new Vector2(0, 0);
+        uv[1] = new Vector2(1, 0);
+        uv[2] = new Vector2(0, 1);
+        uv[3] = new Vector2(1, 1);
+
+        mesh = new Mesh();
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        meshFilter = gameObject.GetComponent<MeshFilter>();
+        
+        if (meshFilter == null)
+        {
+            meshFilter = gameObject.AddComponent<MeshFilter>();
+        }
+
+        meshFilter.mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (animTimeCurrent <= animTimeTarget)
+        {
+            animTimeCurrent += Time.deltaTime;
+            animTimeCurrent = Mathf.Clamp(animTimeCurrent,0,animTimeTarget);
+
+            float currentHeight = (animTimeCurrent / animTimeTarget) * Height;
+
+            vertices[0].y = currentHeight;
+            vertices[1].y = currentHeight;
+            vertices[2].y = currentHeight;
+            vertices[3].y = currentHeight;
+
+            if (mesh != null)
+            {
+                mesh.vertices = vertices;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public enum WorldBuilderDirection
+{
+    North, South, East, West
+}
+
+public class WorldBuilder
+{
+    public WorldBuilder(
+        World world,
+        int x,
+        int y,
+        float height,
+        int health,
+        WorldBuilderDirection direction = WorldBuilderDirection.North)
+    {
+        _world = world;
+        Direction = direction;
+        X = x;
+        Z = y;
+        Height = height;
+        Health = health;
+    }
+    public WorldBuilderDirection Direction { get; set; }
+    public int X { get; set; }
+    public int Z { get; set; }
+    public int Health { get; set; }
+
+    public float ChanceOfSpawningNewBuilder{get;set;} = 0.03f;
+    public float Height { get; set; }
+
+    public float HeightDegredationMin{get;set;} = 1;
+
+    public float HeightDegredationMax{get;set;} = 10;
+
+    public bool Alive => Health > 0;
+
+    public bool IsInWorldBounds => 
+        X >= 0 && X < _world.Dimensions && Z >= 0 && Z < _world.Dimensions;
+
+    public void UpdateMovement()
+    {
+        Health--;
+
+        switch (Direction)
+        {
+            case WorldBuilderDirection.West:
+                X--;
+                break;
+            case WorldBuilderDirection.East:
+                X++;
+                break;
+            case WorldBuilderDirection.South:
+                Z--;
+                break;
+            case WorldBuilderDirection.North:
+                Z++;
+                break;
+            default:
+                break;
+        }
+
+        if (!IsInWorldBounds) // for now we'll just kill the builder if it's out of bounds.
+        {
+            Health = 0; 
+            return;
+        }
+
+        Height -= Random.Range(HeightDegredationMin,HeightDegredationMax);
+
+        UpdateNewBuilderSpawning();
+    }
+
+    private static List<WorldBuilderDirection> GetPerpendicularDirections(
+        WorldBuilderDirection direction)
+    {
+        List<WorldBuilderDirection> results = new List<WorldBuilderDirection>(2);
+
+        switch (direction)
+        {
+            case WorldBuilderDirection.West:
+            case WorldBuilderDirection.East:
+                results.Add(WorldBuilderDirection.North);
+                results.Add(WorldBuilderDirection.South);
+                break;
+            case WorldBuilderDirection.South:
+            case WorldBuilderDirection.North:
+                results.Add(WorldBuilderDirection.East);
+                results.Add(WorldBuilderDirection.West);
+                break;
+            default:
+                break;
+        }
+
+        return results;
+    }
+
+    private void UpdateNewBuilderSpawning()
+    {
+        var directions = GetPerpendicularDirections(Direction);
+        foreach (var dir in directions)
+        {
+            if (Random.value < ChanceOfSpawningNewBuilder)
+            {
+                float newHeight = Height - Random.Range(HeightDegredationMin, HeightDegredationMax);
+                switch (dir)
+                {
+                    case WorldBuilderDirection.West:
+                        _world.MakeWorldBuilder(X - 1, Z, newHeight, Health - 1, dir);
+                        break;
+                    case WorldBuilderDirection.East:
+                        _world.MakeWorldBuilder(X + 1, Z, newHeight, Health - 1, dir);
+
+                        break;
+                    case WorldBuilderDirection.South:
+                        _world.MakeWorldBuilder(X, Z - 1, newHeight, Health - 1, dir);
+                        break;
+                    case WorldBuilderDirection.North:
+                        _world.MakeWorldBuilder(X, Z + 1, newHeight, Health - 1, dir);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    private World _world;
+}
+
+public class World : MonoBehaviour
+{
+    public GameObject cellTemplate;
+    public int Dimensions = 20;
+    public int CellSize = 10;
+    // how long each step animates
+    public float AnimTimeTarget = 1;
+
+    public int WorldGenerationInitialHealth = 5;
+    public float WorldGenerationInitialHeight = 50;
+    public List<GameObject> cellObjects;
+
+        public float Height ;
+
+    public float HeightDegredationMin = 1;
+
+    public float HeightDegredationMax = 10;
+
+    void Start()
+    {
+        cellObjects = new List<GameObject>(Dimensions * Dimensions);
+        for (int i = 0; i < Dimensions * Dimensions; ++i)
+        {
+            cellObjects.Add(null);
+        }
+        InitWorldGeneration(Dimensions / 2, Dimensions / 2);
+    }
+
+    GameObject GetCell(int x, int z) => cellObjects[z * CellSize + x];
+
+    bool AreAnyBuildersAlive() => _builders.Any(x => x.Alive);
+
+    void InitWorldGeneration(int x, int z)
+    {
+        float height = WorldGenerationInitialHeight;
+        int health = WorldGenerationInitialHealth;
+
+        MakeCell(x, z, height);
+        _animTimeCurrent = 0;
+
+        _directionsToBuildLeft.Enqueue(WorldBuilderDirection.West);
+        _directionsToBuildLeft.Enqueue(WorldBuilderDirection.East);
+        _directionsToBuildLeft.Enqueue(WorldBuilderDirection.North);
+        _directionsToBuildLeft.Enqueue(WorldBuilderDirection.South);
+
+        var direction = _directionsToBuildLeft.Dequeue();
+
+        MakeWorldBuilder(x - 1, z, height, health, direction);
+    }
+
+    void MakeCell(int x, int z, float height)
+    {
+        if (cellObjects[z * Dimensions + x] != null)
+        {
+            return;
+        }
+        float xCoord = x * CellSize - (Dimensions / 2) * CellSize;
+        float zCoord = z * CellSize - (Dimensions / 2) * CellSize;
+        Vector3 coord = new Vector3(xCoord, 0, zCoord);
+        var cellObject = Instantiate(cellTemplate, coord, Quaternion.identity);
+        cellObjects[z * Dimensions + x] = cellObject;
+        var cell = cellObject.GetComponent<Cell>();
+        cell.MakeCube(height, CellSize / 2, AnimTimeTarget);
+    }
+
+    public void MakeWorldBuilder(
+        int x,
+        int z,
+        float height,
+        int health,
+        WorldBuilderDirection direction = WorldBuilderDirection.North)
+    {
+        if (x >= 0 && x < Dimensions && z >= 0 && z < Dimensions)
+        {
+            var worldBuilder = new WorldBuilder(this, x, z, height, health, direction);
+            worldBuilder.Height = Height;
+            worldBuilder.HeightDegredationMax = HeightDegredationMax;
+            worldBuilder.HeightDegredationMin = HeightDegredationMin;
+            _builders.Add(worldBuilder);
+        }
+    }
+
+    void StepWorldGeneration()
+    {
+        // we are going to add to the list while modifying so we need to make a temp copy
+        List<WorldBuilder> tempBuilders = new List<WorldBuilder>(_builders);
+        foreach (var builder in tempBuilders )
+        {
+            if (!builder.Alive)
+            {
+                continue;
+            }
+
+            MakeCell(builder.X, builder.Z, builder.Height);
+            builder.UpdateMovement();
+        }
+    }
+
+    void Update()
+    {
+
+        if (!AreAnyBuildersAlive() && _directionsToBuildLeft.Count > 0)
+        {
+            float height = WorldGenerationInitialHeight;
+            int health = WorldGenerationInitialHealth;
+            var dir = _directionsToBuildLeft.Dequeue();
+            int x = Dimensions / 2;
+            int z = Dimensions / 2;
+
+            _builders.Clear();
+
+            switch (dir)
+            {
+                case WorldBuilderDirection.East:
+                    MakeWorldBuilder(x + 1, z, height, health, WorldBuilderDirection.East);
+                    break;
+
+                case WorldBuilderDirection.South:
+                    MakeWorldBuilder(x, z - 1, height, health, WorldBuilderDirection.South);
+                    break;
+
+                case WorldBuilderDirection.North:
+                    MakeWorldBuilder(x, z + 1, height, health, WorldBuilderDirection.North);
+                    break;
+            }
+
+        }
+
+        if (_animTimeCurrent >= AnimTimeTarget && AreAnyBuildersAlive())
+        {
+            StepWorldGeneration();
+            _animTimeCurrent = 0;
+        }
+        else
+        {
+            _animTimeCurrent += Time.deltaTime;
+        }
+    }
+
+    private float _animTimeCurrent;
+
+    private Queue<WorldBuilderDirection> _directionsToBuildLeft = new Queue<WorldBuilderDirection>();
+
+    private HashSet<WorldBuilder> _builders = new HashSet<WorldBuilder>();
+}
