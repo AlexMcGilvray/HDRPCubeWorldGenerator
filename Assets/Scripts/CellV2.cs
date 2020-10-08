e58452a36c87d44788a784d34d074f261bfff338
alex <alex@levelism.com>
2020-10-08 16:08:18 -0700

    checking in my work before getting started on the makecell function implementation.

--- Assets/Scripts/CellV2.cs
+++ Assets/Scripts/CellV2.cs
@@ -13,13 +13,14 @@ public enum CellV2FaceDirection
 
 public class FaceInfo
 {
-
+    public int[] RenderVertexIndices{get;} = new int[4];
+    public int[] LogicalVertexIndices{get;} = new int[4];
+    public Vector3 Normal{get;}
+    public int[] RenderUVIndices {get;} = new int[4];
 }
 
 public class CellV2 : MonoBehaviour
 {
-    public float Height = 1f;
-
     public Material worldMaterial;
 
     void Start()
@@ -46,25 +47,37 @@ public class CellV2 : MonoBehaviour
         }
     }
 
+    public void MakeCell(float size, float height, float animTime = 1f)
+    {
+        _height = height;
+        // populate logical vertex structure
+        
+
+
+        // make each face & setup faceInfos logical struct
+        
+    }
+
+    public void MakeFace(CellV2FaceDirection direction,float size)
+    {
+
+    }
+
     void Update()
     {
 
     }
     // logical model data
-    Vector3[] _logicalVertices = new Vector3[8];
-
-    Dictionary<CellV2FaceDirection, FaceInfo> _faceInfos =
+    private Vector3[] _logicalVertices = new Vector3[8];
+    private Dictionary<CellV2FaceDirection, FaceInfo> _faceInfos =
         new Dictionary<CellV2FaceDirection, FaceInfo>();
+    private float _height;
     // render model data
-    Vector3[] _renderVertices = new Vector3[20]; // 5 faces with 4 vertices each
-    Vector3[] _renderNormals = new Vector3[20];
-    Vector2[] _renderUVs = new Vector2[20];
-    int[] _renderIndices = new int[30]; // 5 faces * (2 triangles * 3 indices)
-
-    MeshFilter _meshFilter;
-
-    Mesh _mesh;
-
-    MeshRenderer _meshRenderer;
-
+    private Vector3[] _renderVertices = new Vector3[20]; // 5 faces with 4 vertices each
+    private Vector3[] _renderNormals = new Vector3[20];
+    private Vector2[] _renderUVs = new Vector2[20];
+    private int[] _renderIndices = new int[30]; // 5 faces * (2 triangles * 3 indices)
+    private MeshFilter _meshFilter;
+    private Mesh _mesh;
+    private MeshRenderer _meshRenderer;
 }
